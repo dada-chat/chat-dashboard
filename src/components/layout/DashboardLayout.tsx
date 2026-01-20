728d720b141bdf7497adfe488307b3b837e154fc
dslee0525 <lds8835@gmail.com>
2026-01-20 10:56:32 +0900

    feat: 대시보드 뷰포트 너비 min-1280으로 수정

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -6,7 +6,7 @@ import ContentHeader from "./ContentHeader";
 import { Viewport } from "next";
 
 export const viewport: Viewport = {
-  width: 1200, // 대시보드, 1200px 폭으로 고정 (적응형)
+  width: 1280, // 대시보드, 1200px 폭으로 고정 (적응형)
   initialScale: 1,
 };
 
@@ -22,7 +22,7 @@ export default function DashboardLayout({
 
   return (
     <>
-      <div className="flex h-screen w-full overflow-hidden">
+      <div className="flex h-screen w-full min-w-7xl overflow-hidden">
         {/* Lnb */}
         <NavigationBar />
         <div className="flex flex-col flex-1 bg-white">
